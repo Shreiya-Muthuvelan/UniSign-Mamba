715b41115c68e91c72147c7c9eb91e64c2c31ddd
Shreiya Muthuvelan <shreiyavel@gmail.com>
2025-12-13 13:12:20 +0400

    Update pose_embeddings_MLP.py

--- preprocessing/pose_extraction/pose_embeddings_MLP.py
+++ preprocessing/pose_extraction/pose_embeddings_MLP.py
@@ -20,39 +20,33 @@ class PoseEmbeddingMLP(nn.Module):
         assert pose.dim() == 2
         return self.mlp(pose)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-POSE_FEATURES_ROOT = os.path.join(BASE_DIR, "pose_features") 
-POSE_EMBEDDINGS_BASE_ROOT = os.path.join(BASE_DIR, "pose_embeddings")
-SUBSETS = ['train', 'dev', 'test']
-os.makedirs(POSE_EMBEDDINGS_BASE_ROOT, exist_ok=True)
+
+POSE_INPUT_ROOT = "phoenix_pose_features_test" #change the path for each train,test and dev splits
+POSE_EMBED_SAVE_ROOT = "phoenix_pose_embeddings_test"
+os.makedirs(POSE_EMBED_SAVE_ROOT, exist_ok=True)
 
 INPUT_DIM = 258
 EMBED_DIM = 512
 pose_model = PoseEmbeddingMLP(input_dim=INPUT_DIM, embed_dim=EMBED_DIM)
 
-for subset in SUBSETS:
-    print(F"Starting embedding for subeset:{subset}")
-    POSE_INPUT_ROOT=os.path.join(POSE_FEATURES_ROOT, subset)
-    POSE_EMBED_SAVE_ROOT=os.path.join(POSE_EMBEDDINGS_BASE_ROOT, subset)
-    os.makedirs(POSE_EMBED_SAVE_ROOT, exist_ok=True)
+pose_files = glob.glob(os.path.join(POSE_INPUT_ROOT, "*.npy"))
+print(f"Found {len(pose_files)} pose files.")
+device="cpu"
+for pose_path in tqdm(pose_files):
+    try:
+        pose_np = np.load(pose_path) # shape: (T, 258)
+        pose_tensor = torch.from_numpy(pose_np).float().to(device)
 
 
+        with torch.no_grad():
+            embeddings = pose_model(pose_tensor) # (T, 512)
 
-    pose_files = glob.glob(os.path.join(POSE_INPUT_ROOT, "*.npy"))
-    print(f"Found {len(pose_files)} pose files.")
-    device="cpu"
-    for pose_path in tqdm(pose_files):
-        try:
-            pose_np = np.load(pose_path)  # shape: (T, 258)
-            pose_tensor = torch.from_numpy(pose_np).float().to(device)
 
-            with torch.no_grad():
-                embeddings = pose_model(pose_tensor)  # (T, 512)
+        base_name = os.path.splitext(os.path.basename(pose_path))[0]
+        save_path = os.path.join(POSE_EMBED_SAVE_ROOT, base_name + ".pt")
+        torch.save(embeddings.cpu(), save_path)
 
-            base_name = os.path.splitext(os.path.basename(pose_path))[0]
-            save_path = os.path.join(POSE_EMBED_SAVE_ROOT, base_name + ".pt")
-            torch.save(embeddings.cpu(), save_path)
-        except Exception as e:
-            print(f"Failed on {pose_path}: {e}")
+    except Exception as e:
+        print(f"Failed on {pose_path}: {e}")
 
-    print(f" Pose embeddings saved in: {POSE_EMBED_SAVE_ROOT}")
+print(f" Pose embeddings saved in: {POSE_EMBED_SAVE_ROOT}")
